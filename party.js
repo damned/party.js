aa360924250fa8521ee58cb26ba2bc6018efe865
danm <writetodan@yahoo.com>
2013-10-12 15:09:52 +0100

    @damned surprisingly got something working fairly quickly

--- party.js
+++ party.js
@@ -1,12 +1,23 @@
 // events requirements
-// 0 - must: clearer than just function replacement
-// 1 - must: just a function call
-// 2 - should: use function not string name of function
-// 3 - nice: specifies contract (i.e. what arguments passed)
-// 4 - nice: multiple listeners
+// 0 - must: clearer than just function replacement vTICK
+// 1 - must: just a function call vTICK
+// 2 - must: be easy to use in tests
+// 3 - should: use function not string name of function vTICK
+// 4 - should: warn if event not listened to
+// 5 - nice: specifies contract (i.e. what arguments passed)
+// 6 - nice: multiple listeners
+// 7 - nice: guard who (which obj) can raise events?
+// 8 - nice: remove raise object - just call directly on events
+// 9 - nice: include events object directly in api of host
+
+var party = require('./lib/events.js')
 
 var host = function() {
   var partygoers = [];
+  var events = party.events({
+    on_arrival: function (person) {},
+    on_ejection: function (person, reason) {}
+  });
 
   function tell(person, message) {
     person.on_told(message)
@@ -14,35 +25,41 @@ var host = function() {
 
   function let_in(person) {
     partygoers.push(person);
-    api.on_arrival(person);
+    events.raise.on_arrival(person);
     tell(person, 'welcome, ' + person.name());
   }
 
   var api = {
-
     greet: function (person) {
       if (!person.is_drunk()) {
         let_in(person);
       }
       else {
-        api.on_ejection(person, person.name() + ' is drunk');
+        events.raise.on_ejection(person, person.name() + ' is drunk');
       }
     },
-    on_arrival: function () { // outgoing-event TBD
-
-    },
-    on_ejection: function () { // outgoing-event TBD
-
+    toString: function() {
+      return 'host object';
     }
   };
+  events.raised_from(api);
   return  api;
 }
 
 var person = function(name, drunk) {
   return {
-    name: function() { return name; },
-    is_drunk: function() { return (drunk !== undefined) },
-    on_told: function(message) { console.log(name + ' hears: "' + message + '"')}
+    name: function() {
+      return name;
+    },
+    is_drunk: function() {
+      return (drunk !== undefined)
+    },
+    on_told: function(message) {
+      console.log(name + ' hears: "' + message + '"')
+    },
+    toString: function() {
+      return 'person - (' + name + ')';
+    }
   };
 }
 
@@ -61,8 +78,8 @@ var grapevine = {
   }
 }
 
-dan.on_arrival = grapevine.spread_arrival;
-dan.on_ejection = grapevine.gossip;
+dan.events.on_arrival = grapevine.spread_arrival;
+dan.events.on_ejection = grapevine.gossip;
 
 
 dan.greet(bob);
