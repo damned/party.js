b8ebd0dbda6e1a93802d3301534b6e48f84fe9fe
danm <writetodan@yahoo.com>
2013-10-12 15:32:41 +0100

    @damned showing that just calling the event function the old way leaves 'this' as raiser/caller of event

--- danparty.js
+++ danparty.js
@@ -4,15 +4,17 @@
 // 2 - must: be easy to use in tests
 // 3 - should: use function not string name of function vTICK
 // 4 - should: warn if event not listened to
-// 5 - nice: specifies contract (i.e. what arguments passed)
-// 6 - nice: multiple listeners
-// 7 - nice: guard who (which obj) can raise events?
-// 8 - nice: remove raise object - just call directly on events
-// 9 - nice: include events object directly in api of host
+// 5 - should: keep same 'this' behaviour as direct call (if possible?)
+// 6 - nice: specifies contract (i.e. what arguments passed)
+// 7 - nice: multiple listeners
+// 8 - nice: guard who (which obj) can raise events?
+// 9 - nice: remove raise object - just call directly on events
+// 10 - nice: include events object directly in api of host (remove 'raised_from')?
 
 var party = require('./party.js')
 
 var host = function() {
+  var api = {};
   var partygoers = [];
   var events = party.events({
     on_arrival: function (person) {},
@@ -25,22 +27,36 @@ var host = function() {
 
   function let_in(person) {
     partygoers.push(person);
-    events.raise.on_arrival(person);
+
+    //old world
+    api.on_arrival(person);
+
+    //new world
+//    events.raise.on_arrival(person);
     tell(person, 'welcome, ' + person.name());
   }
 
-  var api = {
+  api = {
     greet: function (person) {
       if (!person.is_drunk()) {
         let_in(person);
       }
       else {
-        events.raise.on_ejection(person, person.name() + ' is drunk');
+        //new world
+//        events.raise.on_ejection(person, person.name() + ' is drunk');
+
+        //new world
+        api.on_ejection(person, person.name() + ' is drunk');
       }
     },
     toString: function() {
-      return 'host object';
+      return 'the host';
     }
+
+    //old world
+    ,
+    on_arrival: function(){},
+    on_ejection: function(){}
   };
   events.raised_from(api);
   return  api;
@@ -81,9 +97,15 @@ var grapevine = {
   }
 }
 
-dan.events.on_arrival = grapevine.spread_arrival;
-dan.events.on_ejection = grapevine.gossip;
+// new world
+//dan.events.on_arrival = grapevine.spread_arrival;
+//dan.events.on_ejection = grapevine.gossip;
 
+// old world
+dan.on_arrival = grapevine.spread_arrival;
+dan.on_ejection = grapevine.gossip;
+
+grapevine.spread_arrival({ name: function() { return 'calling directly from top level'; }})
 
 dan.greet(bob);
 dan.greet(sue);
