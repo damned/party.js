59deff385ed4f7ef271a4481042f57411726c93a
danm <writetodan@yahoo.com>
2013-10-12 15:12:47 +0100

    @damned renaming - actual source file should be party.js

---
+++ danparty.js
@@ -0,0 +1,87 @@
+// events requirements
+// 0 - must: clearer than just function replacement vTICK
+// 1 - must: just a function call vTICK
+// 2 - must: be easy to use in tests
+// 3 - should: use function not string name of function vTICK
+// 4 - should: warn if event not listened to
+// 5 - nice: specifies contract (i.e. what arguments passed)
+// 6 - nice: multiple listeners
+// 7 - nice: guard who (which obj) can raise events?
+// 8 - nice: remove raise object - just call directly on events
+// 9 - nice: include events object directly in api of host
+
+var party = require('./party.js')
+
+var host = function() {
+  var partygoers = [];
+  var events = party.events({
+    on_arrival: function (person) {},
+    on_ejection: function (person, reason) {}
+  });
+
+  function tell(person, message) {
+    person.on_told(message)
+  }
+
+  function let_in(person) {
+    partygoers.push(person);
+    events.raise.on_arrival(person);
+    tell(person, 'welcome, ' + person.name());
+  }
+
+  var api = {
+    greet: function (person) {
+      if (!person.is_drunk()) {
+        let_in(person);
+      }
+      else {
+        events.raise.on_ejection(person, person.name() + ' is drunk');
+      }
+    },
+    toString: function() {
+      return 'host object';
+    }
+  };
+  events.raised_from(api);
+  return  api;
+}
+
+var person = function(name, drunk) {
+  return {
+    name: function() {
+      return name;
+    },
+    is_drunk: function() {
+      return (drunk !== undefined)
+    },
+    on_told: function(message) {
+      console.log(name + ' hears: "' + message + '"')
+    },
+    toString: function() {
+      return 'person - (' + name + ')';
+    }
+  };
+}
+
+var bob = person('bob'),
+    sue = person('sue'),
+    rita = person('rita', 'drunk');
+
+var dan = host();
+
+var grapevine = {
+  spread_arrival: function(partygoer) {
+    console.log('guess what? ' + partygoer.name() + ' just arrived')
+  },
+  gossip: function(partygoer, why) {
+    console.log('oh, dear: ' + partygoer.name() + ' was not let in because: ' + why)
+  }
+}
+
+dan.events.on_arrival = grapevine.spread_arrival;
+dan.events.on_ejection = grapevine.gossip;
+
+
+dan.greet(bob);
+dan.greet(sue);
+dan.greet(rita);
--- party.js
+++ party.js
@@ -1,87 +1,55 @@
-// events requirements
-// 0 - must: clearer than just function replacement vTICK
-// 1 - must: just a function call vTICK
-// 2 - must: be easy to use in tests
-// 3 - should: use function not string name of function vTICK
-// 4 - should: warn if event not listened to
-// 5 - nice: specifies contract (i.e. what arguments passed)
-// 6 - nice: multiple listeners
-// 7 - nice: guard who (which obj) can raise events?
-// 8 - nice: remove raise object - just call directly on events
-// 9 - nice: include events object directly in api of host
+exports.events = function(events_spec) {
+  var source;
+  var event_names = [];
+  var listeners = {};
 
-var party = require('./lib/events.js')
-
-var host = function() {
-  var partygoers = [];
-  var events = party.events({
-    on_arrival: function (person) {},
-    on_ejection: function (person, reason) {}
-  });
-
-  function tell(person, message) {
-    person.on_told(message)
+  function add_event(event_name) {
+    api.raise[event_name] = function() {
+      console.log('raising event: ' + event_name);
+      listeners[event_name].apply(this, arguments);
+    }
+    Object.defineProperty(api, event_name, {
+      set: function(listener) {
+        console.log('hooking up event: ' + event_name + ' to listener: ' + listener);
+        if (typeof(listener) !== 'function') {
+          throw 'hang on a minute -  that listener is not a function: ' + listener;
+        }
+        listeners[event_name] = listener;
+      }
+    });
   }
 
-  function let_in(person) {
-    partygoers.push(person);
-    events.raise.on_arrival(person);
-    tell(person, 'welcome, ' + person.name());
-  }
+  var api = {};
+  api.raised_from = function(new_source) {
+    source = new_source;
+    source['events'] = api;
+    for (var function_spec in events_spec) {
+      event_names.push(function_spec.toString());
+    }
+    events_spec['__source_of'] = function(name) {
+      return (typeof(this[name]) == 'function') ? this[name].toString() : 'oops, not a function: ' + name;
+    }
 
-  var api = {
-    greet: function (person) {
-      if (!person.is_drunk()) {
-        let_in(person);
-      }
-      else {
-        events.raise.on_ejection(person, person.name() + ' is drunk');
-      }
-    },
-    toString: function() {
-      return 'host object';
+    event_names.forEach(function(event_name) {
+      console.log('event: ' + event_name + ' has parameters: ' + events_spec.__source_of(event_name));
+      console.log('about to define ' + event_name + ' on: ' + api.raise);
+      add_event(event_name);
+    });
+    for (var event in api) {
+      console.log('(events api / events) has property: ' + event)
+    }
+    for (var event in api.raise) {
+      console.log('(events api / events).raise has property: ' + event)
     }
   };
-  events.raised_from(api);
-  return  api;
-}
-
-var person = function(name, drunk) {
-  return {
-    name: function() {
-      return name;
-    },
-    is_drunk: function() {
-      return (drunk !== undefined)
-    },
-    on_told: function(message) {
-      console.log(name + ' hears: "' + message + '"')
-    },
+  api.raise = {
     toString: function() {
-      return 'person - (' + name + ')';
+      return 'party.events.raise object'
     }
   };
+  api.toString = function() {
+    return 'party.events object'
+  };
+  return api;
 }
 
-var bob = person('bob'),
-    sue = person('sue'),
-    rita = person('rita', 'drunk');
-
-var dan = host();
-
-var grapevine = {
-  spread_arrival: function(partygoer) {
-    console.log('guess what? ' + partygoer.name() + ' just arrived')
-  },
-  gossip: function(partygoer, why) {
-    console.log('oh, dear: ' + partygoer.name() + ' was not let in because: ' + why)
-  }
-}
-
-dan.events.on_arrival = grapevine.spread_arrival;
-dan.events.on_ejection = grapevine.gossip;
-
-
-dan.greet(bob);
-dan.greet(sue);
-dan.greet(rita);
