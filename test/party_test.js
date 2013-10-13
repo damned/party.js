c1696554bec30073fbe4878029f2dafd90e996d8
danm <writetodan@yahoo.com>
2013-10-13 09:10:31 +0100

    @damned testing in place for main use case :)

--- test/party_test.js
+++ test/party_test.js
@@ -1,7 +1,29 @@
 var expect = require('chai').expect;
+var party = require('../party.js');
 
 describe('party.js', function() {
-  it('should have a test pass', function() {
-    expect('blah').to.equal("blah");
+  it('should allow host object to raise an event which calls another object', function() {
+    var events = party.events({
+      an_event: function () {}
+    });
+    var sender = {
+      raise_an_event: function () {
+        events.raise.an_event();
+      }
+    }
+    events.raised_from(sender);
+
+    var receiver = {
+      was_called: false,
+      target: function () {
+        this.was_called = true;
+      }
+    };
+
+    sender.events.an_event.calls(receiver).target;
+
+    sender.raise_an_event();
+
+    expect(receiver.was_called).to.be.true;
   })
 })
